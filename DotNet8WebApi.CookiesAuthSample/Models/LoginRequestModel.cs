ce7dad0697ced39f0f179b2856d814861c12332c
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 17:46:29 +0630

    Format Code

--- DotNet8WebApi.CookiesAuthSample/Models/LoginRequestModel.cs
+++ DotNet8WebApi.CookiesAuthSample/Models/LoginRequestModel.cs
@@ -1,8 +1,7 @@
-﻿namespace DotNet8WebApi.CookiesAuthSample.Models
+﻿namespace DotNet8WebApi.CookiesAuthSample.Models;
+
+public class LoginRequestModel
 {
-    public class LoginRequestModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
